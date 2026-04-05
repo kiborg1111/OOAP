ef63ab743210af6994788766058bffe648d04e53
Вячеслав <gorbunovslava2006@gmail.com>
2026-04-06 03:19:16 +1000

    Add lab-3

---
+++ VectorEngine/src/App.tsx
@@ -0,0 +1,46 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AnimatePresence, motion } from 'framer-motion';
+import Gallery from './screens/Gallery';
+import Editor from './screens/Editor';
+import './index.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="bg-black min-h-screen">
+        <AnimatePresence mode="wait">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 0.2 }}
+                >
+                  <Gallery />
+                </motion.div>
+              }
+            />
+            <Route
+              path="/editor/:id"
+              element={
+                <motion.div
+                  initial={{ opacity: 0, x: 50 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  exit={{ opacity: 0, x: -50 }}
+                  transition={{ duration: 0.2 }}
+                >
+                  <Editor />
+                </motion.div>
+              }
+            />
+          </Routes>
+        </AnimatePresence>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
